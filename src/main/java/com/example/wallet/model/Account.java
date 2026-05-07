5112f5c280deb49599edd71939ee2a14e983f6f5
Magdalena Furman <madzia912@gmail.com>
2026-05-07 18:57:15 +0200

    Update Account.java

--- src/main/java/com/example/wallet/model/Account.java
+++ src/main/java/com/example/wallet/model/Account.java
@@ -1,5 +1,6 @@
 package com.example.wallet.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,6 +11,7 @@ import java.math.BigDecimal;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Account {
 
     @Id
